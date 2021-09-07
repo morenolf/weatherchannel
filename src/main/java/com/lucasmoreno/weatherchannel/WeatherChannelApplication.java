5c094724e7abeae137f3d5ddbc2d2988f2ef2bf6
morenolf <l_f_moreno@hotmail.com>
2021-09-07 15:33:55 -0300

    Initial commit for Weather Channel application

---
+++ src/main/java/com/lucasmoreno/weatherchannel/WeatherChannelApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoreno.weatherchannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherChannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherChannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmoreno/weatherchannel/WeatherChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoreno.weatherchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherChannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

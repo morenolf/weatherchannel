75965cc16099a466f1558dbef2e6449219f20204
morenolf <l_f_moreno@hotmail.com>
2021-09-07 22:17:11 -0300

    Creating service with logic to handle solar system behavior and forecast.

---
+++ src/main/java/com/lucasmoreno/weatherchannel/services/SolarSystem.java
@@ -0,0 +1,12 @@
+package com.lucasmoreno.weatherchannel.services;
+
+/**
+ * 
+ * Interface for Solar system for multiple possible system.
+ * 
+ * @author Lucas Moreno
+ *
+ */
+public interface SolarSystem {
+
+}
---
+++ src/main/java/com/lucasmoreno/weatherchannel/services/impl/SolarSystemImpl.java
@@ -0,0 +1,49 @@
+package com.lucasmoreno.weatherchannel.services.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucasmoreno.weatherchannel.builder.impl.PlanetBuilder;
+import com.lucasmoreno.weatherchannel.director.SolarSystemDirector;
+import com.lucasmoreno.weatherchannel.dto.CartesianCoordinatesDto;
+import com.lucasmoreno.weatherchannel.dto.PlanetDto;
+import com.lucasmoreno.weatherchannel.services.SolarSystem;
+
+/**
+ * Service that handles Solar system logic and his construction as well in order
+ * to calculate forecast.
+ * 
+ * @author Lucas Moreno
+ *
+ */
+@Service
+public class SolarSystemImpl implements SolarSystem {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SolarSystemImpl.class);
+
+	private List<PlanetDto> planets;
+
+	@Autowired
+	private SolarSystemDirector solarSystemDirector;
+
+	/**
+	 * Creates the Solar system based on builder pattern using known planets and
+	 * their properties
+	 */
+	public SolarSystemImpl() {
+		this.planets = new ArrayList<>();
+
+		PlanetBuilder planetBuilder = new PlanetBuilder();
+		solarSystemDirector.constructBetasoides(planetBuilder);
+		this.planets.add(planetBuilder.getResult());
+		solarSystemDirector.constructVulcan(planetBuilder);
+		this.planets.add(planetBuilder.getResult());
+		solarSystemDirector.constructBetasoides(planetBuilder);
+		this.planets.add(planetBuilder.getResult());
+	}
+
+}

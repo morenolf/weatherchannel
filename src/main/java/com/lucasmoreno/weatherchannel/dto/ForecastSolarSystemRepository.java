2656fb7e2f00a14bb1ad285ff0a239ca053f20e5
morenolf <l_f_moreno@hotmail.com>
2021-09-09 12:33:26 -0300

    Creating Forecast classes to persist and model.

--- src/main/java/com/lucasmoreno/weatherchannel/dto/ForecastSolarSystemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lucasmoreno.weatherchannel.dto;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ForecastSolarSystemRepository extends JpaRepository<ForecastSolarSystemDto, Long>{
-
-	
-}
